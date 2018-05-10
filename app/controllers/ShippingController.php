44937543a57280b8824c298715d105d494d216b6
ivansmarchuk <ivans.marchuk@gmail.com>
2018-05-10 22:23:36 +0200

    new category router, shows all products for the each category

---
+++ app/controllers/ShippingController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ivan Marchuk
+ * Date: 10.05.2018
+ */
+
+namespace app\controllers;
+
+
+use coffeeshop\base\Controller;
+use RedBeanPHP\R;
+
+class ShippingController extends Controller
+{
+    public function shippingAction(){
+
+    }
+}
